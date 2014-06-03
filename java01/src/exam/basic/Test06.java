cf420c2845d5ab48f2ba7cf93e41786cdc9f45a6
bit <bit@bit-PC>
2014-06-03 15:38:49 +0900

    .

---
+++ java01/src/exam/basic/Test06.java
@@ -0,0 +1,17 @@
+/* 6. 자바의 기본 연산은 int 이다.
+ * - byte 끼리의 연산 결과도 int
+ * - short 끼리의 연산 결과 int
+ * - long 끼리의 연산 결과 long
+ * - { byte, short, char} > int > long > float > double
+ */
+package exam.basic;
+
+public class Test06 {
+
+	public static void main(String[] args) {
+		byte b1 = 32; 	// 상수인 경우 변수에 저장할 수 있다면 승인해줌
+		
+
+	}
+
+}
