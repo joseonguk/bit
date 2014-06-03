c5909f2ffc985a38dc2bec71bbb34fdbc793d8b2
bit <bit@bit-PC>
2014-06-03 20:16:02 +0900

    .

---
+++ java01/src/exam/basic/Test.java
@@ -0,0 +1,165 @@
+package exam.basic;
+
+public class Test {
+	public static void main(String[] args) {
+	 /* 1.다음 중 main메서드의 선언부로 알맞은 것은(모두 고르시오)?
+		 a. public static void main(String[] as)
+		 b. static public void main(String[] args)
+		 c. public static void main(String args[])
+		 d. public void static main(String[] args)*/
+		
+		/*2. 다음 중 형변환을 생략할 수 있는 것은?
+			byte b = 20;
+			char ch = 'A';
+			int i = 100;
+			
+			a. ch = (char)b;
+			b. b = (byte)i;
+			c. b = (byte)ch;
+			d. i = (int)b;*/
+		
+		/*3. 실행 결과 x의 값과 y의 값 temp의 값을 구하여라!
+			int x = 24;
+			int y = 14;	
+			int temp = x-- + ++y;
+			
+			a. x = 24, y = 14, temp = 39
+			b. x = 23, y = 15, temp = 39
+			c. x = 24, y = 14, temp = 38
+			d. x = 23, y = 15, temp = 39*/
+			
+		/*4. 다음중 패키지 선언 방식이 맞는 것은??
+			a. exam/basic
+			b. study/basic
+			c. exam.basic
+			d. exam.!basic*/
+		
+		/*5. 실행 결과 x의 값과 sum의 값을 구하여라
+			int x = 10;
+			int sum = 0;
+			sum = --x + x++ + x++;
+
+			a. x = 10, sum = 27
+			b. x = 11, sum = 27
+			c. x = 10, sum = 28
+			d. x = 11, sum = 28*/
+		
+		/*6. 다음 실행 결과의 값을 구하여라.
+			int x = 5;
+			int y = 3;
+			System.out.println(x | y);
+			
+			a. 6
+			b. 7
+			c. 8
+			d. 9*/
+		
+		//7. 다음 중 변수 선언이 바른 것은?
+				
+			/*a. int x = NULL;
+			b. int x = class;
+			c. int x = if;
+			d. int x = *a
+			*/ 
+		
+		/*8. 다음 실행 결과값을 구하여라
+			float x = 6.3f;
+			int y = x / 3;
+		
+			System.out.println(y);
+		
+			a. 컴파일 에러
+			b. 2
+			c. 2.1
+			*/
+		/*9. y = x + z 에서 피연산자는 무엇인가?
+			a. =, +
+			b. y, x, z
+			c. y, =, x, z*/
+		
+			
+		/*10. 다음 실행 결과값을 구하여라 
+			int x = 12L;
+			int y = x / 3;
+			
+			System.out.println(y);
+
+			a. 4
+			b. 컴파일 에러
+			c. 컴파일 에러는 뜨나 4가 출력
+			*/
+		
+		
+		
+		/*11. 다음 실행 결과 나오는 결과값을 구하여라 
+			int a = 10;
+			int b = 20;
+			boolean bo = a != b;
+			System.out.println(bo);*/
+	
+		/*12. byte b = -11; 의 2진수를 구하여라
+					1000 1011*/
+		
+		/*13. 19의 이진수를 2의 보수를 구하여라(2진수로)
+					10100*/
+					
+		/*14. byte 타입의 변수에 저장될 수 있는 값의 범위는?
+				-128 ~ 127*/
+		
+		/*15. 출력값을 구하여라?
+		 		byte b = 30;
+				short s = 40;
+				int i = 50;
+				double d = 20.40;
+				double sum = b + s + i + d;
+				
+				System.out.println(sum);
+		 */
+		
+		/*16. 다음 실행결과를 나타내어라 ?
+				byte i = -13;
+				
+				System.out.println(i >> 2);
+				
+			-4
+		*/
+			
+		/*17. 다음 실행결과를 나타내어라.
+			int x = 130;
+			x -=200;
+			
+			System.out.println(x);
+			
+			-70*/
+			
+		/*18. 다음 실행결과를 나타내어라.
+			int x = 7;
+			x >>= 2;
+			
+			System.out.println(x);
+			
+			1*/
+		
+		/*19. 다음의 실행결과를 적으시오.
+			int x = 15;
+			int y = 22;
+	
+			int b = x > y ? x : y;
+	
+			System.out.println(b);
+			
+			22
+			*/
+		
+		/*20. 이 클래스의 실행결과를 적으시오. 만약 실행이 안된다면 그 이유는 무엇인가?
+			class TestClass {
+				 int i = 10; 
+				 
+				 {
+					 int x = 20;
+				 }
+				System.out.println(x); 
+			}*/
+			 			
+  }
+}
